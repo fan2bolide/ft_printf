2af89ed79a549f76c112e6eaf1263b0be7ffab72
basil <basiljeannot@icloud.com>
2022-11-24 16:47:12 +0100

    first commit

---
+++ ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/24 16:43:00 by bajeanno          #+#    #+#             */
+/*   Updated: 2022/11/24 16:45:36 by bajeanno         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_printf(const char *format, ...);
+
+#endif
