1a219941a06f37075a39bc37aa6f31ecb8281688
basil <basiljeannot@icloud.com>
2022-11-24 18:37:51 +0100

    update

--- ft_printf.c
+++ ft_printf.c
@@ -6,9 +6,24 @@
 /*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:42:41 by bajeanno          #+#    #+#             */
-/*   Updated: 2022/11/24 16:42:51 by bajeanno         ###   ########lyon.fr   */
+/*   Updated: 2022/11/24 17:57:38 by bajeanno         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	ft_printf(const char *format, ...)
+{
+	size_t	i;
+	size_t	size;
+
+	size = 0;
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+			size += ft_print_value(format[++i]);
+		i++;
+	}
+	return (size);
+}
--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,21 @@
 /*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:43:00 by bajeanno          #+#    #+#             */
-/*   Updated: 2022/11/24 16:45:36 by bajeanno         ###   ########lyon.fr   */
+/*   Updated: 2022/11/24 17:57:25 by bajeanno         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <stdlib.h>
+# include <stddef.h>
 # include <unistd.h>
 
-void	ft_printf(const char *format, ...);
+size_t	ft_printf(const char *format, ...);
+size_t	ft_print_char(char c,);
+size_t	ft_print_string(char *str);
+size_t 	ft_print_ptr(void *ptr);
+size_t	
+
 
 #endif
