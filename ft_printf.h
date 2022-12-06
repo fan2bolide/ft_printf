512791b1feea71530086dfef27f755bfccef11d4
Basile Jeannot <bajeanno@z1r4p3.42lyon.fr>
2022-12-07 00:18:10 +0100

    removed useless include

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 16:43:00 by bajeanno          #+#    #+#             */
-/*   Updated: 2022/12/06 19:39:35 by bajeanno         ###   ########lyon.fr   */
+/*   Updated: 2022/12/07 00:16:23 by bajeanno         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 # define FT_PRINTF_H
 # include "libft.h"
 # include <stdarg.h>
-# include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-int		ft_printf(const char *format, ...);
-int		ft_print_addr(void *ptr);
-int		ft_print_int(int nb);
-int		ft_print_unsigned(unsigned int nb);
-int		ft_print_hexa(int nb);
-int		ft_print_hexa_capslock(int nb);
-int		ft_putnbr_base(int nb, char *base);
-int		ft_putnbr_base_unsigned(size_t nb, char *base);
+int	ft_printf(const char *format, ...);
+int	ft_print_addr(void *ptr);
+int	ft_print_int(int nb);
+int	ft_print_unsigned(unsigned int nb);
+int	ft_print_hexa(int nb);
+int	ft_print_hexa_capslock(int nb);
+int	ft_putnbr_base(int nb, char *base);
+int	ft_putnbr_base_unsigned(size_t nb, char *base);
 
 #endif
